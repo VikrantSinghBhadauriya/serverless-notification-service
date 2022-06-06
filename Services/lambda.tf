34ecd0413457ecee8822683223e5f2fa438fc463
feyzee <11005389+feyzee@users.noreply.github.com>
2022-06-06 14:10:01 +0530

    Updated Cloudwatch logs resources

--- Services/lambda.tf
+++ Services/lambda.tf
@@ -7,7 +7,7 @@ data "archive_file" "zip_lambda" {
 
 resource "aws_lambda_function" "lambda_func_1" {
   filename         = "${path.module}/function/myzip/lambda_func_1.zip"
-  function_name    = "lambda_func_1"
+  function_name    = var.function_1
   role             = aws_iam_role.poc_iam_for_lambda_1.arn
   source_code_hash = data.archive_file.zip_lambda["lambda_func_1"].output_base64sha256
   handler          = "lambda_func_1.lambda_handler"
@@ -22,7 +22,7 @@ resource "aws_lambda_function" "lambda_func_1" {
 
 resource "aws_lambda_function" "lambda_func_2" {
   filename         = "${path.module}/function/myzip/lambda_func_2.zip"
-  function_name    = "lambda_func_2"
+  function_name    = var.function_2
   role             = aws_iam_role.poc_iam_for_lambda_2.arn
   source_code_hash = data.archive_file.zip_lambda["lambda_func_2"].output_base64sha256
   handler          = "lambda_func_2.lambda_handler"
@@ -51,11 +51,11 @@ resource "aws_lambda_permission" "test" {
 }
 
 resource "aws_cloudwatch_log_group" "lambda_1" {
-  name              = "poc-spring-lambda-1-logs"
+  name              = "/aws/lambda/${var.function_1}"
   retention_in_days = 90
 }
 
 resource "aws_cloudwatch_log_group" "lambda_2" {
-  name              = "poc-spring-lambda-2-logs"
+  name              = "/aws/lambda/${var.function_2}"
   retention_in_days = 90
 }
--- Services/variables.tf
+++ Services/variables.tf
@@ -1,5 +1,16 @@
 variable "lambda_funtions" {
   type    = set(string)
   default = ["lambda_func_1", "lambda_func_2"]
+}
 
+variable "function_1" {
+  type        = string
+  description = "Lambda function 1 name"
+  default     = "lambda_func_1"
+}
+
+variable "function_2" {
+  type        = string
+  description = "Lambda function 2 name"
+  default     = "lambda_func_2"
 }
