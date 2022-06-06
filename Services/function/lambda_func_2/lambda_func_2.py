1dc94f120c9e72ab072a34a08b20451d8a351c96
feyzee <11005389+feyzee@users.noreply.github.com>
2022-06-06 12:04:57 +0530

    Added logging, env vars, exception handling. Splitted ndjson loop into seperate function. Formatted with black.

--- Services/function/lambda_func_2/lambda_func_2.py
+++ Services/function/lambda_func_2/lambda_func_2.py
@@ -1,24 +1,51 @@
 import json
 import boto3
+import logging
+from os import environ
+from botocore.exceptions import ClientError
+
+s3 = boto3.client("s3")
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def ndjson(file_object):
+    """Splits NDJSON file file_object JSON array"""
+    ndjson_content = file_object["Body"].read().decode("utf-8")
+    result = []
+
+    for ndjson_line in ndjson_content.splitlines():
+        if not ndjson_line.strip():
+            continue  # ignore empty lines
+        json_line = json.loads(ndjson_line)
+        result.append(json_line)
+
+    return result
+
+
 def lambda_handler(event, context):
-    s3 = boto3.client("s3")
-    if event:
-        print("Event:  ", event)
-        data = event["Records"][0]["body"]
-        print("Data : ", data)
-        fileObj = s3.get_object(Bucket='poc-spring-test-bucket', Key=data)
-        ndjson_content = fileObj["Body"].read().decode('utf-8')
-        print(ndjson_content)
-        result = []
-        for ndjson_line in ndjson_content.splitlines():
-            print("Conerting ndjson to json")
-            if not ndjson_line.strip():
-               continue  # ignore empty lines
-            json_line = json.loads(ndjson_line)
-            result.append(json_line)
-        print("Result: ", result)
-        s3.put_object(
-          Body=str(json.dumps(result)),
-          Bucket='poc-spring-test-bucket',
-          Key='file.txt'
-        )
+    """Lambda Handler"""
+
+    logger.info(f"Event data - {event}")
+
+    bucket = environ["bucket-name"]
+    object_prefix = environ["processed-prefix"]
+    data = event["Records"][0]["body"].strip('"')
+
+    try:
+        file_object = s3.get_object(Bucket=bucket, Key=data)
+    except ClientError as e:
+        logger.error(f"An error occured while accesing S3 object - {e}")
+    else:
+        json_array = ndjson(file_object)
+        new_data = {"Content": json_array}
+
+        try:
+            response = s3.put_object(
+                Body=str(json.dumps(new_data)),
+                Bucket=bucket,
+                Key=f"{object_prefix}/{data}",
+            )
+        except ClientError as e:
+            logger.error(f"An error occured while object the file - {e}")
+            logger.error(f"Response from AWS API - {response}")
